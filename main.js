e52ac1b1da294dc58eb5c559d320df6a43d20f61
inesbdl <bouadilin@gmail.com>
2024-11-16 17:54:07 +0100

    Retirer le dossier client de l'index Git

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
+      nodeIntegration: true, // ou false selon ton besoin
+      contextIsolation: false, // ou true pour plus de sécurité
     },
   });
 
-  mainWindow.loadFile('index.html');
+  // Si tu es en développement, charge Nuxt via localhost
+  if (process.env.NODE_ENV !== 'production') {
+    mainWindow.loadURL('http://localhost:3000');
+  } else {
+    // En production, charge le fichier généré par Nuxt
+    mainWindow.loadFile(path.join(__dirname, 'client/.output/public/index.html'));
+  }
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
