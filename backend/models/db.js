835e4d05bc523ead3c07bdd0e216a5b3ed571083
OswinFreyr <pisanotanguy@gmail.com>
2025-04-01 16:31:40 +0200

    reset main

--- backend/models/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const db = new Sequelize({
-  dialect: 'sqlite',
-  storage: '../database/bookshelf.sqlite',
-  logging: false
-});
-
-module.exports = { db };
